8afe2dee0ee28e6ea4d90c5bd3f3b0f4a0ca34ce
abduhelel <timehelel@163.com>
2020-11-26 04:31:01 +0800

    eureka客户端配置成功

--- 01-springcloud-service-provider/src/main/java/com/xunao/springcloud/Application.java
+++ 01-springcloud-service-provider/src/main/java/com/xunao/springcloud/Application.java
@@ -2,8 +2,10 @@ package com.xunao.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient   //开启客户端
 public class Application {
 
 	public static void main(String[] args) {
